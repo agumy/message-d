2acad15b2823474d4e6b98fad5d536eab38b5268
agumy <nov13sh@gmail.com>
2021-04-04 16:43:44 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const config = {
+  entry: {
+    "content-scripts/index": "./src/content-scripts/index.ts",
+    "event-pages/index": "./src/event-pages/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src")],
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+};
+
+module.exports = config;
