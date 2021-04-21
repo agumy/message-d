2e4dbec2da18564ef685c593e880900ab630b48c
agumy <nov13sh@gmail.com>
2021-04-21 23:40:21 +0900

    Improve Translation

--- src/content-scripts/index.ts
+++ src/content-scripts/index.ts
@@ -59,9 +59,8 @@ const selectTranslationTarget = async (event: KeyboardEvent): Promise<void> => {
     document.body.appendChild(loading);
 
     const translationTarget = currentTarget.innerHTML;
-    const lineBreaked = translationTarget
-      .replaceAll(/\. /g, "$&\n")
-      .replaceAll(/<[a-zA-Z](.*?[^?])?>/g, "\n$&");
+    const lineBreaked = translationTarget.replaceAll(/\. /g, "$&\n");
+    // .replaceAll(/<[a-zA-Z](.*?[^?])?>/g, "\n$&");
 
     const key = await sha256(lineBreaked);
     watingTranslation.push({
