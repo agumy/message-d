d4ca19e937aff6caa25e33b8d4f19aecb1d01568
agumy <nov13sh@gmail.com>
2021-05-01 23:19:54 +0900

    set options to storage

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,53 +1,24 @@
-const createListItem = (value: string) => {
-  const li = document.createElement("li");
-  li.classList.add("list-item");
+import { buildIgnoreList } from "./ignoreList";
+import { setToStorage } from "./storage";
 
-  const selector = document.createElement("span");
-  selector.textContent = value;
-  li.appendChild(selector);
-
-  const box = document.createElement("div");
-  box.classList.add("box");
-  li.appendChild(box);
-
-  const deleteButton = document.createElement("span");
-  deleteButton.classList.add("dli-close");
-  box.appendChild(deleteButton);
-
-  return li;
-};
-
-const ignore = (e: Event) => {
+const ignore = async (e: Event) => {
   e.preventDefault();
-  const input = document.querySelector(".selector");
-  const ul = document.querySelector(".ignore-list");
-
-  if (
-    !(input instanceof HTMLInputElement) ||
-    !(ul instanceof HTMLUListElement)
-  ) {
-    return;
-  }
+  const input = document.querySelector(".selector") as HTMLInputElement;
 
   if (!input.value) {
     return;
   }
 
-  const li = createListItem(input.value);
-  ul.appendChild(li);
-
-  const box = li.querySelector(".box")!;
-  box.addEventListener("click", () => {
-    input.focus();
-    li.remove();
-  });
+  await setToStorage(input.value);
+  buildIgnoreList();
 
   input.value = "";
   input.focus();
 };
 
 (() => {
-  (document.querySelector(".selector") as HTMLInputElement).focus();
   const form = document.querySelector(".form")!;
   form.addEventListener("submit", ignore);
+  buildIgnoreList();
+  (document.querySelector(".selector") as HTMLInputElement).focus();
 })();
