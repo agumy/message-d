454add588eeaf19203675f0660f1c5ce06d36456
agumy <nov13sh@gmail.com>
2021-05-13 22:10:15 +0900

    Improve getAllNode

--- src/content-scripts/getAllTextNode.ts
+++ src/content-scripts/getAllTextNode.ts
@@ -11,21 +11,15 @@ const htmlTagsNoTranslate = [
 ];
 const htmlTagsInlineIgnore = ["BR", "KBD", "WBR"];
 
-const elements: Element[] = [];
-
 const hasChildOnlyTextNode = (node: Node) =>
-  node.childNodes.length &&
+  Boolean(node.childNodes.length) &&
   Array.from(node.childNodes).every((node) => node.nodeType === 3);
 
 const hasChildOnlyTextNodeRecursive = (
   node: Node,
   deps: number = 0
 ): boolean => {
-  if (node instanceof Element && node.innerHTML.length > 1000) {
-    return false;
-  }
-
-  if (node.nodeType === 3) {
+  if (node.nodeType === 3 && (node.textContent ?? "").trim().length > 0) {
     return true;
   }
 
@@ -33,19 +27,14 @@ const hasChildOnlyTextNodeRecursive = (
     return true;
   }
 
-  if (deps === 2) {
-    return false;
-  }
-
   if (!node.childNodes.length) {
     return false;
   }
 
-  if (hasChildOnlyTextNode(node)) {
-    return true;
-  }
-
-  if (node instanceof Element && node.innerHTML.length < 250) {
+  if (
+    hasChildOnlyTextNode(node) &&
+    (node as Element).innerHTML.length <= 5000
+  ) {
     return true;
   }
 
@@ -57,22 +46,23 @@ const hasChildOnlyTextNodeRecursive = (
 export const getAllTextNodeConsideringSelector = async (
   target: Element = document.body
 ) => {
+  const elements: (Node | Element)[] = [];
+  // console.log(target);
   const { selectors } = await getSelectorStorage();
   const joinedSelector = selectors.join(",");
   const ignoredElements = Array.from(target.querySelectorAll(joinedSelector));
 
-  const getAllTextNode = (target: Element): Element[] => {
+  const getAllTextNode = (target: Node | Element): (Node | Element)[] => {
     const shouldPush = hasChildOnlyTextNodeRecursive(target);
     if (shouldPush) {
       elements.push(target);
     } else {
       for (const node of Array.from(target.childNodes)) {
         if (
-          node.nodeType === 1 &&
           !htmlTagsNoTranslate.includes(node.nodeName.toUpperCase()) &&
           !ignoredElements.some((e) => e.isEqualNode(node))
         ) {
-          getAllTextNode(node as Element);
+          getAllTextNode(node);
         }
       }
     }
