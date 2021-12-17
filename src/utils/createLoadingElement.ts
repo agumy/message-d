7c362c61274c6392f1c0f4d68114e737dfc29e42
agumy <nov13sh@gmail.com>
2021-12-17 17:52:03 +0900

    fix loading element

--- src/utils/createLoadingElement.ts
+++ src/utils/createLoadingElement.ts
@@ -1,7 +1,10 @@
 export const createLoadingElement = (): HTMLDivElement => {
+  const loadingWrapper = document.createElement("div");
+  loadingWrapper.classList.add("message-d__loading-wrapper");
   const loading = document.createElement("div");
   loading.classList.add("message-d__loader");
   loading.id = "message-d__loader-id";
+  loadingWrapper.appendChild(loading);
 
-  return loading;
+  return loadingWrapper;
 };
